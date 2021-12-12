844fc43ab1ec0ada07af2d09db8994a5f5b2f2b1
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2021-12-12 23:38:30 +0530

    Add Love.py to project

---
+++ Love.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+# outline color : black
+
+# Object fill color : red
+
+color("black", "red")
+
+begin_fill()
+
+left(50)
+
+forward(100)
+
+circle(40, 180)
+
+left(260)
+
+circle(40, 180)
+
+forward(100)
+
+end_fill()
+
+done()
